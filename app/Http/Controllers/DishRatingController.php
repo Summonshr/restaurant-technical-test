78e95b481e6e63ce531acc2e105989a5b2f9f900
Suman Shrestha <summonshr@gmail.com>
2023-07-29 20:40:27 +0545

    Add DishRatingController with rateDish method

---
+++ app/Http/Controllers/DishRatingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RateDishRequest;
+use App\Models\Dish;
+
+class DishRatingController extends Controller
+{
+    public function rateDish(RateDishRequest $request, Dish $dish)
+    {
+
+        $user = $request->user();
+
+        if ($user->nickname === '_Sméagol_') {
+            return response()->json(['message' => 'Sméagol cannot rate dishes at The Dancing Pony.'], 403);
+        }
+
+        if ($dish->ratings()->where('user_id', $user->id)->exists()) {
+            return response()->json(['message' => 'You have already rated this dish.'], 400);
+        }
+
+        $rating = $request->input('rating');
+
+        $dish->ratings()->create(['rating' => $rating, 'user_id' => $user->id]);
+
+        return response()->json(['message' => 'Dish rated successfully']);
+    }
+}
