b2ac2b8cf793dd26a93a0f36af0c335f21b87f41
Suman Shrestha <summonshr@gmail.com>
2023-07-29 20:42:17 +0545

    Add API routes and controllers for dishes and dish ratings

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DishController;
+use App\Http\Controllers\DishRatingController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware(['auth:sanctum', 'throttle:20,1',])->group(function () {
+    Route::resource('/dishes', DishController::class);
+    Route::post('/dishes/{dish}/rate', [DishRatingController::class, 'rateDish']);
+});
+
+Route::post('/login', [AuthController::class, 'login']);
