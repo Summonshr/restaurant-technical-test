b265e22ca2fa04034eac9df232f08e22134e1a3b
Suman Shrestha <summonshr@gmail.com>
2023-07-29 20:40:46 +0545

    Add dish request classes for create, update, destroy, and rate dishes

---
+++ app/Http/Requests/StoreDishRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDishRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|unique:dishes',
+            'description' => 'required|unique:dishes',
+            'image' => 'nullable|url',
+            'price' => 'required|numeric|min:0',
+        ];
+    }
+}
